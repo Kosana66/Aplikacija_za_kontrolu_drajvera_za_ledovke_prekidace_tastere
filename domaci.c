753c8aafcdfb7398273182d0721e26570f832d49
Kosana66 <80408276+Kosana66@users.noreply.github.com>
2022-11-15 13:32:00 +0100

    Update domaci.c
    Dodato softversko diferenciranje za tastere

--- domaci.c
+++ domaci.c
@@ -7,9 +7,10 @@ int main ()
 {
 	FILE *fp;
 	unsigned int br1, br2, res;
+        unsigned int novo, staro = 0;
 	char *str;
 	char lval1,lval2,lval3,lval4;
-	char tval1,tval2,tval3,tval4;
+	char tval4;
 	char sval1,sval2,sval3,sval4;
 	size_t num_of_bytes = 6;
 
@@ -45,63 +46,7 @@ int main ()
 
 		printf("Vrednosti ledovke su: %d %d %d %d \n", lval1, lval2, lval3, lval4);
 	
-
-		//Citanje vrednosti tastera
-		fp = fopen ("/dev/button", "r");
-		if(fp==NULL)
-		{
-			puts("Problem pri otvaranju /dev/button\n");
-			return -1;
-		}
-
-		str = (char *)malloc(num_of_bytes+1); 
-		getline(&str, &num_of_bytes, fp); 
-
-		if(fclose(fp))
-		{
-			puts("Problem pri zatvaranju /dev/button\n");
-			return -1;
-		}
-
-		tval1 = str[2] - 48;
-		tval2 = str[3] - 48;
-		tval3 = str[4] - 48;
-		tval4 = str[5] - 48;
-		free(str);
-		
-		printf("Vrednosti tastera su: %d %d %d %d \n", tval1, tval2, tval3, tval4);
-		
-
-		//Citanje vrednosti prekidaca
-		fp = fopen ("/dev/switch", "r");
-		if(fp==NULL)
-		{
-			puts("Problem pri otvaranju /dev/switch\n");
-			return -1;
-		}
-
-		str = (char *)malloc(num_of_bytes+1); 
-		getline(&str, &num_of_bytes, fp); 
-
-		if(fclose(fp))
-		{
-			puts("Problem pri zatvaranju /dev/switch\n");
-			return -1;
-		}
-
-		sval1 = str[2] - 48;
-		sval2 = str[3] - 48;
-		sval3 = str[4] - 48;
-		sval4 = str[5] - 48;
-		free(str);
-
-		printf("Vrednosti prekidaca su: %d %d %d %d \n", sval1, sval2, sval3, sval4);
-	
-
-		printf("---------------------------------------\n");
-
-
-		if(lval1==0 && lval2==0 && lval3==0 && lval4==0 )
+                if(lval1==0 && lval2==0 && lval3==0 && lval4==0 )
 			br1=0;
 		else if(lval1==0 && lval2==0 && lval3==0 && lval4==1 )
 			br1=1;
@@ -134,8 +79,34 @@ int main ()
 		else if(lval1==1 && lval2==1 && lval3==1 && lval4==1 )
 			br1=15;
 
-		printf(" broj 1 je :  %d  \n", br1);
+                printf(" broj 1 je :  %d  \n", br1);
 
+		//Citanje vrednosti prekidaca
+		fp = fopen ("/dev/switch", "r");
+		if(fp==NULL)
+		{
+			puts("Problem pri otvaranju /dev/switch\n");
+			return -1;
+		}
+
+		str = (char *)malloc(num_of_bytes+1); 
+		getline(&str, &num_of_bytes, fp); 
+
+		if(fclose(fp))
+		{
+			puts("Problem pri zatvaranju /dev/switch\n");
+			return -1;
+		}
+
+		sval1 = str[2] - 48;
+		sval2 = str[3] - 48;
+		sval3 = str[4] - 48;
+		sval4 = str[5] - 48;
+		free(str);
+
+		printf("Vrednosti prekidaca su: %d %d %d %d \n", sval1, sval2, sval3, sval4);
+	
+		
 		if(sval1==0 && sval2==0 )
 			br2=0;
 		else if(sval1==0 && sval2==1 )
@@ -159,12 +130,36 @@ int main ()
 
 		printf(" resenje je :  %d  \n", res);
 
-		if(tval4 == 1)
-			br1=res;
 
-		sleep(1);
+                //Citanje vrednosti tastera
+		fp = fopen ("/dev/button", "r");
+		if(fp==NULL)
+		{
+			puts("Problem pri otvaranju /dev/button\n");
+			return -1;
+		}
 
+		str = (char *)malloc(num_of_bytes+1); 
+		getline(&str, &num_of_bytes, fp); 
 
+		if(fclose(fp))
+		{
+			puts("Problem pri zatvaranju /dev/button\n");
+			return -1;
+		}
+
+		tval4 = str[5] - 48;
+		free(str);
+		
+		printf("Vrednosti tastera 0 je: %d \n", tval4);
+		
+
+                novo=0;
+		if(tval4)
+			novo=1;
+                if( novo!=0 && novo!=staro ) 
+                {
+                         
 
 		fp = fopen ("/dev/led", "w");
 		if(fp==NULL)
@@ -210,7 +205,15 @@ int main ()
 		{
 			puts("Problem pri zatvaranju /dev/led tokom upisa\n");
 			return -1;
-		}
+		} 
+                    
+                } 
+                staro=novo;
+
+
+                printf("---------------------------------------\n");
+
+ 
 
 	}
 }
